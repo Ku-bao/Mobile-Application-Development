861c088b5527c29aba0b29653e020979f571fc36
陈睿阳 <ch2233614468@gmail.com>
2024-04-08 17:19:04 +0800

    add the menu in toolbar with rounded corners

--- app/src/main/java/com/example/securenotes/MainActivity.java
+++ app/src/main/java/com/example/securenotes/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.securenotes;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.Language) {
+            return true;
+        }
+        if (id == R.id.Theme) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
